24c6ba66922c1b983a8687547fc8d912e04569a7
rnbirinci <rananil05@gmail.com>
2026-07-21 18:54:39 +0300

    feat: Added seed data for devices and users. 1 device and 4 users(1 admin, 3 residents). Added the test data to the README file. Added a new command to hash the pin for the users. The hashed pin will be printed in the console.

---
+++ backend/cmd/hashpin/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+func main() {
+	pins := []string{"0123", "1234", "2345", "3456"}
+
+	for _, pin := range pins {
+		hash, err := bcrypt.GenerateFromPassword([]byte(pin), bcrypt.DefaultCost)
+		if err != nil {
+			log.Fatal("Error", err)
+		}
+		fmt.Println(pin, string(hash))
+	}
+}
---
+++ backend/migrations/002_seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO devices (name, address, power, location, price) VALUES ('SUHUBE EVLERİ 22kW-1', 'Kemalpaşa Mahallesi, 130. Cd, Bina No: 15', 22, '40.752439, 30.355499', 9.9);
+
+INSERT INTO users (name, surname, role, license_plate, pin_hash) VALUES
+('Ali','Yılmaz','admin','34ABC000','$2a$10$kOLiByH5iNIIGukvt2uc8.3KBH5UVJw5NoXnJloXmSrXhE3Ds9JfS'),
+('Veli','Kaya','resident','34ABC001','$2a$10$zW4IPGZlj2oxxCW93UXge.GXcBlSKZg5/CcqNvyHd.LQafZ6fvyq6'),
+('Ayşe','Demir','resident','34ABC002','$2a$10$cWDgLxE5chsjCvggEaPB0.6uuMbibllpdEELv/AJktoGNIC55CaRe'),
+('Fatma','Şahin','resident','34ABC003','$2a$10$CzAzA.GbRKaUvmPQy7z82.IG1NzVW5bfrRmVic5pRCZxTP45LyZQa');
